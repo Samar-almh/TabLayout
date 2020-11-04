e1770239fc516c7ec3b0c7eea92dac5359eb39d8
Samar.almh <49631872+Samar-almh@users.noreply.github.com>
2020-11-04 21:08:30 +0200

    update my challenge

--- app/src/main/java/com/samar/tablayout/HomeFragment.kt
+++ app/src/main/java/com/samar/tablayout/HomeFragment.kt
@@ -5,23 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment() {
 
+    private var param1: Int? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        }
-
+        arguments?.let {
+            param1 = it.getInt(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        } }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val  view = inflater.inflate(R.layout.fragment_home, container, false)
+        if (param1 == 0) {
+            view.text_fragment.text= getString(R.string.home)
+            view.home_layout.setBackgroundResource(R.color.pink) }
+        else if(param1 == 1){
+            view.text_fragment.text= getString(R.string.food)
+            view.home_layout.setBackgroundResource(R.color.yellow) }
+        else if (param1 == 2){
+            view.text_fragment.text= getString(R.string.hotel)
+            view.home_layout.setBackgroundResource(R.color.purple) }
+        else
+        {view.text_fragment.text= getString(R.string.home)}
+        return view
     }
 
-   
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: Int, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
--- app/src/main/java/com/samar/tablayout/MainActivity.kt
+++ app/src/main/java/com/samar/tablayout/MainActivity.kt
@@ -1,32 +1,73 @@
 package com.samar.tablayout
 
-import android.icu.lang.UCharacter.GraphemeClusterBreak.V
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+private lateinit var tabLayout: TabLayout
+private lateinit var tabViewPager: ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpTabs()
+        tabLayout = findViewById(R.id.taps)
+        tabViewPager = findViewById(R.id.pager)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, tab?.text.toString(), Toast.LENGTH_LONG).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+        tabViewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    0 -> HomeFragment.newInstance(position, "")
+                    1 -> HomeFragment.newInstance(position, "")
+                    2 -> HomeFragment.newInstance(position, "")
+                    else ->HomeFragment.newInstance(position, "")
+                }
+            }
+
+            override fun getItemCount(): Int {
+                return 3
+
+
+            }
+        }
+
+        TabLayoutMediator(tabLayout, tabViewPager) { tab, position ->
+            val tab1 = when (position) {
+                0 -> {
+                    tab.text = "Home"
+                    tab.setIcon(R.drawable.ic_baseline_local_airport_24)
+                }
+                1 -> {
+                    tab.text = "Food"
+                    tab.setIcon(R.drawable.ic_baseline_local_dining_24)
+                }
+                2 -> {
+                    tab.text = "Hotel"
+                    tab.setIcon(R.drawable.ic_baseline_home_work_24)
+                }
+                else -> null
+            }
+
+
+        }.attach()
     }
-
-    private fun setUpTabs() {
-        val adapter = ViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(HomeFragment(), "Travel")
-        adapter.addFragment(SettingsFragment(), "Food")
-        adapter.addFragment(FavouritesFragment(), "Hotel")
-        viewPager.adapter = adapter
-        tabs.setupWithViewPager(viewPager)
-
-        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_local_airport_24)
-        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_local_dining_24)
-        tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_home_work_24)
-    }
-
-
 }
